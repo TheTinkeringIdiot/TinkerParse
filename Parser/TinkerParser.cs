1ece1748d0f380cdec531b1fc2066bf462dad2a6
Tinkering Idiot <admin@tinkeringidiot.com>
2024-02-17 15:35:12 -0500

    parse icons

--- Parser/TinkerParser.cs
+++ Parser/TinkerParser.cs
@@ -25,32 +25,46 @@ namespace Parser
 
             List<Item> items = new List<Item>();
             List<NanoProgram> nanos = new List<NanoProgram>();
+            List<Icon> icons = new List<Icon>();
 
             int count = 0;
 
-            Console.WriteLine("Parsing Items...");
-            foreach(KeyValuePair<int, ulong> kvp in rdbController.RecordTypeToId[(int)RdbRecordType.Item])
+            // Console.WriteLine("Parsing Items...");
+            // foreach(KeyValuePair<int, ulong> kvp in rdbController.RecordTypeToId[(int)RdbRecordType.Item])
+            // {
+            //     if(count % 5000 == 0) { Console.WriteLine($"Parsed items: {count}"); }
+            //     count++;
+            //     BinaryReader reader = rdbController.Get((int)RdbRecordType.Item, kvp.Key);
+            // BinaryReader reader = rdbController.Get((int)RdbRecordType.Item, 305554);
+            // Item item = new Item();
+            // item.PopulateFromStream(reader);
+            //     items.Add(item);
+            // }
+
+            // Console.WriteLine("Parsing Nanos...");
+            // count = 0;
+            // foreach(KeyValuePair<int, ulong> kvp in rdbController.RecordTypeToId[(int)RdbRecordType.NanoProgram])
+            // {
+            //     if(count % 1000 == 0) { Console.WriteLine($"Parsed items: {count}"); }
+            //     count++;
+            //     BinaryReader reader = rdbController.Get((int)RdbRecordType.NanoProgram, kvp.Key);
+            //     NanoProgram nano = new NanoProgram();
+            //     nano.PopulateFromStream(reader);
+            //     nanos.Add(nano);
+            // }
+
+            Console.WriteLine("Parsing Icons...");
+            count = 0;
+            foreach(KeyValuePair<int, ulong> kvp in rdbController.RecordTypeToId[(int)RdbRecordType.Icon])
             {
-                if(count % 5000 == 0) { Console.WriteLine($"Parsed items: {count}"); }
+                if(count % 1000 == 0) { Console.WriteLine($"Parsed items: {count}"); }
                 count++;
-                BinaryReader reader = rdbController.Get((int)RdbRecordType.Item, kvp.Key);
-                // BinaryReader reader = rdbController.Get((int)RdbRecordType.Item, 305554);
-                Item item = new Item();
-                item.PopulateFromStream(reader);
-                items.Add(item);
+                BinaryReader reader = rdbController.Get((int)RdbRecordType.Icon, kvp.Key);
+                Icon icon = new Icon();
+                icon.PopulateFromStream(reader);
+                icons.Add(icon);
             }
 
-            Console.WriteLine("Parsing Nanos...");
-            count = 0;
-            foreach(KeyValuePair<int, ulong> kvp in rdbController.RecordTypeToId[(int)RdbRecordType.NanoProgram])
-            {
-                if(count % 5000 == 0) { Console.WriteLine($"Parsed items: {count}"); }
-                count++;
-                BinaryReader reader = rdbController.Get((int)RdbRecordType.NanoProgram, kvp.Key);
-                NanoProgram nano = new NanoProgram();
-                nano.PopulateFromStream(reader);
-                nanos.Add(nano);
-            }
 
             
 
